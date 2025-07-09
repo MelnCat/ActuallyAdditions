abf2b234ea6c87b86031a9f875c53f67698e0f6e
MelnCat <melncatuwu@gmail.com>
2025-07-09 11:56:38 -0700

    merge

--- src/main/java/de/ellpeck/actuallyadditions/mod/inventory/ActuallyContainers.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/inventory/ActuallyContainers.java
@@ -2,8 +2,8 @@ package de.ellpeck.actuallyadditions.mod.inventory;
 
 import de.ellpeck.actuallyadditions.mod.ActuallyAdditions;
 import net.minecraft.world.inventory.MenuType;
-import net.neoforged.bus.api.IEventBus;
 import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
